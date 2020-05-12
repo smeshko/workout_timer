fd6a044ccb78787dbfc3c3314ef8abe87a028937
Ivaylo Tsonev <itsonev@ebay.com>
2020-05-12 11:56:01 +0200

    Create tabbed root view

--- WorkoutTimer/App/SceneDelegate.swift
+++ WorkoutTimer/App/SceneDelegate.swift
@@ -8,20 +8,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   var window: UIWindow?
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    let contentView = TimerView(
-      store: Store<TimerState, TimerAction>(
-        initialState: TimerState(
-//          sets: PickerState(value: 2),
-//          workoutTime: PickerState(value: 60),
-//          breakTime: PickerState(value: 20)
-        ),
-        reducer: timerReducer,
-        environment: TimerEnvironment(
-          mainQueue: DispatchQueue.main.eraseToAnyScheduler(),
-          soundClient: .live
-        )
-      )
-    )
+    let contentView = RootView()
     
     if let windowScene = scene as? UIWindowScene {
       let window = UIWindow(windowScene: windowScene)
---
+++ WorkoutTimer/Root/RootView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+import TimerCore
+import ComposableArchitecture
+
+struct RootView: View {
+  var body: some View {
+    TabView {
+      TimerView()
+        .tabItem {
+          Image(systemName: "timer")
+          Text("Quick timer")
+      }
+    }
+  }
+}
+
+struct RootView_Previews: PreviewProvider {
+  static var previews: some View {
+    RootView()
+  }
+}
+
+extension TimerView {
+  init() {
+    self.init(
+      store: Store<TimerState, TimerAction>(
+        initialState: TimerState(),
+        reducer: timerReducer,
+        environment: TimerEnvironment(
+          mainQueue: DispatchQueue.main.eraseToAnyScheduler(),
+          soundClient: .live
+        )
+      )
+    )
+  }
+}
