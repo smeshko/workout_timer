624c6f7525540ca9d853bbe9fea9c813454047e8
Ivaylo Tsonev <itsonev@ebay.com>
2020-05-17 20:36:34 +0200

    Fix test bundle

--- InternalFrameworks/WorkoutCore/Tests/ExerciseSetTests.swift
+++ InternalFrameworks/WorkoutCore/Tests/ExerciseSetTests.swift
@@ -2,42 +2,39 @@ import XCTest
 @testable import WorkoutCore
 
 class ExerciseSetTests: XCTestCase {
-  
-  let pushUp = Exercise(title: "Push up")
-  let jacks = Exercise(title: "Jumping Jacks")
 
   func testAlternating() {
-    let sets = ExerciseSet.alternating([
-      pushUp : 30,
-      jacks : 60
-    ], count: 2)
+    let sets = ExerciseSet.alternating(2, [
+      .pushUps : 30,
+      .jumpingJacks : 60
+    ])
     
     let expectedSets = [
-      ExerciseSet(exercise: pushUp, duration: 30),
-      ExerciseSet(exercise: jacks, duration: 60),
-      ExerciseSet(exercise: pushUp, duration: 30),
-      ExerciseSet(exercise: jacks, duration: 60)
+      ExerciseSet(exercise: .pushUps, duration: 30),
+      ExerciseSet(exercise: .jumpingJacks, duration: 60),
+      ExerciseSet(exercise: .pushUps, duration: 30),
+      ExerciseSet(exercise: .jumpingJacks, duration: 60)
     ]
     
     XCTAssertEqual(sets, expectedSets)
   }
   
   func testBatchWithPause() {
-    let sets = ExerciseSet.sets(2, exercise: jacks, duration: 60, pauseInBetween: 15)
+    let sets = ExerciseSet.sets(2, exercise: .jumpingJacks, duration: 60, pauseInBetween: 15)
     let expected = [
-      ExerciseSet(exercise: jacks, duration: 60),
+      ExerciseSet(exercise: .jumpingJacks, duration: 60),
       .recovery(15),
-      ExerciseSet(exercise: jacks, duration: 60)
+      ExerciseSet(exercise: .jumpingJacks, duration: 60)
     ]
     
     XCTAssertEqual(sets, expected)
   }
   
   func testBatchWithoutPause() {
-    let sets = ExerciseSet.sets(2, exercise: jacks, duration: 60)
+    let sets = ExerciseSet.sets(2, exercise: .jumpingJacks, duration: 60)
     let expected = [
-      ExerciseSet(exercise: jacks, duration: 60),
-      ExerciseSet(exercise: jacks, duration: 60)
+      ExerciseSet(exercise: .jumpingJacks, duration: 60),
+      ExerciseSet(exercise: .jumpingJacks, duration: 60)
     ]
     
     XCTAssertEqual(sets, expected)
