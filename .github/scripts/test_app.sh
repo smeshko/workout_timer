1d5a452a6d0ec6a999eeab4909dcf66b5055fbc0
Ivaylo Tsonev <itsonev@ebay.com>
2020-06-09 19:30:00 +0200

    Test

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -project WorkoutTimer.xcodeproj \
-            -scheme QuickTimer \
+            -scheme WorkoutTimer \
             -destination platform=iOS\ Simulator,OS=13.5,name=iPhone\ 11 \
             clean test | xcpretty
