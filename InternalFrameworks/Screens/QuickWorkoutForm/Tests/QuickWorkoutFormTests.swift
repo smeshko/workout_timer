1924421373888eb95c73624d4ae406a12abb384f
Ivaylo Tsonev <itsonev@ebay.com>
2020-10-24 23:32:01 +0200

    Remove core interface tests

--- InternalFrameworks/Screens/QuickWorkoutForm/Tests/QuickWorkoutFormTests.swift
+++ InternalFrameworks/Screens/QuickWorkoutForm/Tests/QuickWorkoutFormTests.swift
@@ -6,22 +6,20 @@ import CorePersistence
 class QuickWorkoutFormTests: XCTestCase {
 
     func testFlow() {
-        let store = TestStore(
-            initialState: PickerState(),
-            reducer: pickerReducer,
-            environment: PickerEnvironment()
-        )
-
-        let store2 = TestStore(
-            initialState: AddTimerSegmentState(id: UUID()),
-            reducer: addTimerSegmentReducer,
-            environment: AddTimerSegmentEnvironment(uuid: UUID.init)
-        )
-
-        let store3 = TestStore(
-            initialState: CreateQuickWorkoutState(),
-            reducer: createQuickWorkoutReducer,
-            environment: CreateQuickWorkoutEnvironment(mainQueue: AnyScheduler(DispatchQueue.testScheduler), repository: .mock)
-        )
+//        let store = TestStore(
+//            initialState: CreateQuickWorkoutState(),
+//            reducer: createQuickWorkoutReducer,
+//            environment: CreateQuickWorkoutEnvironment(
+//                mainQueue: AnyScheduler(DispatchQueue.testScheduler),
+//                repository: .mock
+//            )
+//        )
+//
+//        store.assert(
+//            .send(.updateName("My workout")) {
+//                $0.name = "My workout"
+//            },
+//            .send(.addTimerSegmentAction(id: UUID(), action: .addSegments))
+//        )
     }
 }
