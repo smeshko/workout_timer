050aac54c5ad05d581228bfd880db7982ddc02f4
Ivaylo Tsonev <itsonev@ebay.com>
2020-12-02 22:34:27 +0100

    Add tokens

---
+++ InternalFrameworks/Core/CoreInterface/Source/DesignSystem/SpacingToken.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+public extension CGFloat {
+    static let xxs: CGFloat = 4
+    static let xs: CGFloat = 8
+    static let s: CGFloat = 12
+    static let m: CGFloat = 16
+    static let l: CGFloat = 18
+    static let xl: CGFloat = 22
+    static let xxl: CGFloat = 28
+}
