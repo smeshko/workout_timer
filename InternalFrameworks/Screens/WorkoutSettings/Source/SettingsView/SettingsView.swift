dedf3013e8053d009561414b4f1677c03394cf24
Ivaylo Tsonev <itsonev@ebay.com>
2021-02-04 16:56:02 +0100

    Simplify code

--- InternalFrameworks/Screens/WorkoutSettings/Source/SettingsView/SettingsView.swift
+++ InternalFrameworks/Screens/WorkoutSettings/Source/SettingsView/SettingsView.swift
@@ -40,37 +40,34 @@ public struct SettingsView: View {
 
                 Section(header: Text("Support")) {
                     if canSendEmail {
-                        Button(action: {
+                        Button("Report a bug") {
                             viewStore.send(.sendBugReport)
-                        }, label: {
-                            Text("Report a bug")
-                        })
+                        }
                         .buttonStyle(PlainButtonStyle())
 
-                        Button(action: {
-                            viewStore.send(.sendFeatureRequest)
-                        }, label: {
-                            Text("Feature request")
-                        })
+                        Button("Feature request") {
+                            viewStore.send(.sendBugReport)
+                        }
                         .buttonStyle(PlainButtonStyle())
                     }
-                    Button(action: {
+
+                    Button("Rate the app") {
                         if let windowScene = UIApplication.shared.windows.first?.windowScene {
                             SKStoreReviewController.requestReview(in: windowScene)
                         }
-                    }, label: {
-                        Text("Rate the app")
-                    })
+                    }
                     .buttonStyle(PlainButtonStyle())
                 }
 
                 Section(header: Text("Legal")) {
-                    Text("Privacy policy")
-                    Button(action: {
+                    Button("Privacy policy") {
+
+                    }
+                    .buttonStyle(PlainButtonStyle())
+
+                    Button("Licenses") {
                         isPresentingLicenses = true
-                    }, label: {
-                        Text("Licenses")
-                    })
+                    }
                     .buttonStyle(PlainButtonStyle())
                 }
 
@@ -97,38 +94,7 @@ public struct SettingsView: View {
                 }
             })
             .sheet(isPresented: $isPresentingLicenses, content: {
-                NavigationView {
-                    TextEditor(
-                        text: .constant(
-                            """
-                            The Composable Architecture
-
-                            MIT License
-
-                            Copyright (c) 2020 Point-Free, Inc.
-
-                            Permission is hereby granted, free of charge, to any person obtaining a copy
-                            of this software and associated documentation files (the "Software"), to deal
-                            in the Software without restriction, including without limitation the rights
-                            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-                            copies of the Software, and to permit persons to whom the Software is
-                            furnished to do so, subject to the following conditions:
-
-                            The above copyright notice and this permission notice shall be included in all
-                            copies or substantial portions of the Software.
-
-                            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-                            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-                            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-                            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-                            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-                            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-                            SOFTWARE.
-                            """
-                        )
-                    )
-                    .navigationTitle("Licenses")
-                }
+                LicensesView()
             })
             .navigationTitle("Settings")
         }
